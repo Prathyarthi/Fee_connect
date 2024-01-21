36cfed1bd88f133284aa428d62695337348ef139
Prathyarthi <prathyarti@gmail.com>
2024-01-21 21:06:03 +0530

    Added DB connection

---
+++ backend/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`)
+        console.log(`MongoDb connected! HOST : ${connectionInstance.connection.host}`);
+    } catch (e) {
+        console.log("MongoDB connection error", e);
+        process.exit(1)
+    }
+}
+
+export default connectToDB
